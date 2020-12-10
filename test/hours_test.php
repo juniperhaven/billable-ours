9e0e582c7176214484469205f73527251667def3
Haven <50467530+juniperhaven@users.noreply.github.com>
2020-12-10 11:40:02 -0800

    Update hours_test.php

--- test/hours_test.php
+++ test/hours_test.php
@@ -6,16 +6,52 @@ require_once 'simpletest/web_tester.php';
 
 class HoursForm extends WebTestCase {
 
-	  function testCorrectPassword() {
+	  function testCorrectHours() {
 		$this->get(VIRTUAL_PATH . '/hours.php');
 		$this->assertResponse(200);
 
-    $this->setField("hours", "2");
+    		$this->setField("hours", "2");
 		$this->setField("rate", "50");
 		$this->clickSubmit("Show Pay");
 
 		$this->assertResponse(200);
 		$this->assertText("You input 2 hours at a rate of $50 and your pay is $100");
+	}
+	
+	function testDecimalHours() {
+		$this->get(VIRTUAL_PATH . '/hours.php');
+		$this->assertResponse(200);
+
+		$this->setField("hours", "1.5"); //decimal number for hours
+		$this->setField("rate", "50");
+		$this->clickSubmit("Show Pay");
+
+		$this->assertResponse(200);
+		$this->assertText("You input 1.5 hours at a rate of $50 and your pay is $75");
+	}
+	
+	function testZeroHours() {
+		$this->get(VIRTUAL_PATH . '/hours.php');
+		$this->assertResponse(200);
+
+		$this->setField("hours", "0"); //decimal number for hours
+		$this->setField("rate", "50");
+		$this->clickSubmit("Show Pay");
+
+		$this->assertResponse(200);
+		$this->assertText("You cannot enter 0 for billable hours");
+	}
+	
+	function testZeroPay() {
+		$this->get(VIRTUAL_PATH . '/hours.php');
+		$this->assertResponse(200);
+
+		$this->setField("hours", "2"); //decimal number for hours
+		$this->setField("rate", "0");
+		$this->clickSubmit("Show Pay");
+
+		$this->assertResponse(200);
+		$this->assertText("You cannot enter 0 for hourly rate");
 	}
 
 }
